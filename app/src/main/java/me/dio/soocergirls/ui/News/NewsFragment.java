3a8ed3e7ec395735c4cbdc3c427ab2959661b576
suellenmuniz <su09muniz@hotmail.com>
2022-07-25 20:28:16 -0300

    first commit

---
+++ app/src/main/java/me/dio/soocergirls/ui/News/NewsFragment.java
@@ -0,0 +1,37 @@
+package me.dio.soocergirls.ui.News;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+
+import me.dio.soocergirls.databinding.FragmentNewsBinding;
+
+public class NewsFragment extends Fragment {
+
+    private FragmentNewsBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        NewsViewModel newsViewModel =
+                new ViewModelProvider(this).get(NewsViewModel.class);
+
+        binding = FragmentNewsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        newsViewModel.getText().observe(getViewLifecycleOwner(), binding.textNews::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
