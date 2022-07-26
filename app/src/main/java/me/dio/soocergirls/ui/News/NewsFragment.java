88ddfbbb060abda11b453446b01350726e42c25a
suellenmuniz <su09muniz@hotmail.com>
2022-07-25 22:38:10 -0300

    Created CardView and improving layout

--- app/src/main/java/me/dio/soocergirls/ui/News/NewsFragment.java
+++ app/src/main/java/me/dio/soocergirls/ui/News/NewsFragment.java
@@ -9,9 +9,11 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 
 import me.dio.soocergirls.databinding.FragmentNewsBinding;
+import me.dio.soocergirls.ui.adapter.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
@@ -25,7 +27,10 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        newsViewModel.getText().observe(getViewLifecycleOwner(), binding.textNews::setText);
+        binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
+        newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
+            binding.rvNews.setAdapter(new NewsAdapter(news));
+        });
         return root;
     }
 
--- app/src/main/java/me/dio/soocergirls/ui/News/NewsViewModel.java
+++ app/src/main/java/me/dio/soocergirls/ui/News/NewsViewModel.java
@@ -1,19 +1,34 @@
 package me.dio.soocergirls.ui.News;
 
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dio.soocergirls.domain.News;
+
 public class NewsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<News>> news;
 
     public NewsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is news fragment");
+        this.news = new MutableLiveData<>();
+
+        //TODO Remover Mock de Noticias
+        List<News> news = new ArrayList<>();
+        news.add(new News("Flamengo tem Desfalque Importante","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+        news.add(new News("Palmeiras joga no Sabado","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+        news.add(new News("Copa do Mundo Feminina Está terminando ","Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."));
+
+        this.news.setValue(news);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<List<News>> getNews() {
+        return news;
     }
 }
+
+
